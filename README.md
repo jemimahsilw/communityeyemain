ab7f76d9a2fff6f18679c4e6ebd25f4ef4ed1240
Jemimah Silwamba <bis21-jsilwamba@mubas.ac.mw>
2026-04-08 11:50:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello people,
+
+Community eye organization is a legal firm organization that suports the non state actors in accessing justice and expressing fully their human rights. 
---
+++ function.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    let slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    function showSlide(index){
+        slides.forEach(slide => slide.classList.remove('active'));
+        slides[index].classList.add('active');
+    }
+
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }, 5000);
+});
